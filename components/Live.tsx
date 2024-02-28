e9bb7de7de1ba5bca11b2723e4ea6a42217242be
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-28 01:19:42 -0600

    open and close chat

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,6 +1,8 @@
 import { useMyPresence, useOthers } from "@/liveblocks.config"
 import { LiveCursors } from "./cursor/LiveCursors"
-import { useCallback } from "react"
+import { useCallback, useEffect, useState } from "react"
+import { CursorChat } from "./cursor/CursorChat"
+import { CursorMode, CursorState } from "@/types/type"
 
 function Live() {
 
@@ -8,18 +10,21 @@ function Live() {
     const [ myPresence, updateMyPresence ] = useMyPresence()
     const { cursor } = myPresence as any
 
+    const [cursorState, setCursorState] = useState<CursorState>({
+        mode: CursorMode.Hidden
+    })
+
     const handlePointerMove = useCallback((e: React.PointerEvent) => {
         e.preventDefault()
         
         const x = e.clientX - e.currentTarget.getBoundingClientRect().x
         const y = e.clientY - e.currentTarget.getBoundingClientRect().y
 
-        console.log({x, y})
         updateMyPresence({ cursor: { x, y }})
     }, [])
 
     const handlePointerLeave = useCallback((e: React.PointerEvent) => {
-        e.preventDefault()
+        setCursorState({ mode: CursorMode.Hidden })
         
         updateMyPresence({ cursor: null, message: null})
     }, [])
@@ -31,6 +36,37 @@ function Live() {
         const y = e.clientY - e.currentTarget.getBoundingClientRect().y
         updateMyPresence({ cursor: { x, y }})
     }, [])
+
+    useEffect(() => {
+        const onKeyUp = (e: KeyboardEvent) => {
+            console.log(cursorState, CursorMode.Chat )
+            if (e.key === '/') {
+                                setCursorState({ 
+                    mode: CursorMode.Chat,
+                    previousMessage: null,
+                    message: ''
+                })
+            }else if (e.key === 'Escape') {
+                updateMyPresence({ message: '' })
+                setCursorState({ mode: CursorMode.Hidden })
+            }
+        }
+
+        const onKeyDown = (e: KeyboardEvent) => {
+            if ( e.key === '/') {
+                e.preventDefault()
+            }
+        }
+    
+        window.addEventListener('keyup', onKeyUp)
+        window.addEventListener('keydown', onKeyDown)
+
+        return () => {
+            window.removeEventListener('keyup', onKeyUp)
+            window.removeEventListener('keydown', onKeyDown)
+        }
+    }, [ updateMyPresence ])
+    
     
     return (
         <div
@@ -39,6 +75,16 @@ function Live() {
             onPointerDown={ handlePointerDown }
             className="border-2 border-green-500 h-full w-full flex justify-center items-center text-center"
         >
+            {
+                cursor && (
+                    <CursorChat 
+                        cursor={ cursor }
+                        cursorState={ cursorState }
+                        setCursorState={ setCursorState }
+                        updateMyPresence={ updateMyPresence }
+                    />
+                )
+            }
             <LiveCursors others={others}/>
         </div>
     )
