475ff6eb7cb5446918fc022879f502a2d5e2730c
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-28 01:58:10 -0600

    add reaction component

---
+++ components/reaction/ReactionButton.tsx
@@ -0,0 +1,19 @@
+
+export function ReactionButton(
+    {
+        reaction,
+        onSelect,
+    }: {
+        reaction: string;
+        onSelect: (reaction: string) => void;
+    }
+) {
+    return (
+        <button
+        className="transform select-none p-2 text-xl transition-transform hover:scale-150 focus:scale-150 focus:outline-none"
+        onPointerDown={() => onSelect(reaction)}
+        >
+        {reaction}
+        </button>
+    );
+}
