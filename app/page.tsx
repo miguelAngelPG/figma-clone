a8d35c48ba3cef6766d6581d566a6a233af6b43d
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-02-28 16:41:05 -0600

    add several references to control shapes into canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,48 @@
 'use client'
 
+import { fabric } from "fabric";
+import { useEffect, useRef } from "react";
 import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
+import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
 
 export default function Page() {
+
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const fabricRef = useRef<fabric.Canvas | null>(null)
+    const isDrawing = useRef(false)
+    const shapeRef = useRef<fabric.Object | null>(null)
+    const selectedShapeRef = useRef<string | null>('rectangle') 
+
+    useEffect(() => {
+        const canvas = initializeFabric({canvasRef, fabricRef})
+        console.log('dkm', canvas)
+        canvas.on('mouse:dblclick', (options) => {
+            console.log('first', options)
+            handleCanvasMouseDown({
+                options,
+                isDrawing,
+                canvas,
+                shapeRef,
+                selectedShapeRef
+            })
+        })
+
+        window.addEventListener('resize', () => {
+            console.log('resize')
+            handleResize({  canvas: fabricRef.current })
+        })
+
+    }, [])
+
     return (
         <main className="h-screen overflow-hidden ">
             <Navbar/>
             <section className="flex flex-row h-full">
                 <LeftSidebar/>
-                <Live/>
+                <Live canvasRef={ canvasRef }/>
                 <RightSidebar/>
             </section>
         </main>
